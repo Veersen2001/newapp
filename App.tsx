1eb0c5fff090b0a4b466bb605f9bb51239d8a7a7
Veerbahadur Sen <veerbahadursen@gmail.com>
2023-08-31 21:18:37 +0530

    my First App

---
+++ App.tsx
@@ -0,0 +1,23 @@
+
+import React from 'react';
+
+import {Button, Text,View} from 'react-native';
+// Text,View etc..=> component
+
+
+
+
+function App(): JSX.Element {
+  
+
+  return (
+    // it is like a div tag
+    <View >
+      <Text style={{ fontSize: 40 }}>Hello My Name is Veerbahadur Sen</Text>
+      <Button title="Press here "></Button>
+    </View>
+  );
+}
+
+
+export default App;
